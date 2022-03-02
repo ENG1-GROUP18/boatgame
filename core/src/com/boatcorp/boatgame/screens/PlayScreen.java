3b3daceb3fd24f0b21a24d4a720fb81679fb2e15
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-02 13:17:40 +0000

    Update PlayScreen.java

--- core/src/com/boatcorp/boatgame/screens/PlayScreen.java
+++ core/src/com/boatcorp/boatgame/screens/PlayScreen.java
@@ -2,6 +2,7 @@ package com.boatcorp.boatgame.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
