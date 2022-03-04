b673ec598213e58ae06aa152a510f44f7eae851b
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-04 20:28:07 +0000

    added points

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -1,14 +1,18 @@
 package com.boatcorp.boatgame;
 import com.badlogic.gdx.math.Vector2;
 
+import java.util.ArrayList;
+
 public class GameState {
-    private Vector2 playerPosition;
+    private Vector2 playerPosition = new Vector2(100,100);
+    private int points;
 
     public GameState() {
-        this(new Vector2(100,100));
+        this(new Vector2(100,100), 0);
     }
-    public GameState(Vector2 playerPosition){
+    public GameState(Vector2 playerPosition, int points){
         this.playerPosition = playerPosition;
+        this.points = points;
     }
 
     public void setPlayerPosition(Vector2 newPosition){
@@ -19,4 +23,7 @@ public class GameState {
         return playerPosition;
     }
 
+    public int getPoints() {
+        return points;
+    }
 }
