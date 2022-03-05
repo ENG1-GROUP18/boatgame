4b8841d2f62c0cd7d239a513db23ad593ad413a1
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-05 14:14:20 +0000

    Update GameState.java

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -1,8 +1,7 @@
 package com.boatcorp.boatgame;
 import com.badlogic.gdx.math.Vector2;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+
+import java.util.*;
 
 public class GameState{
     public Vector2 playerPosition = new Vector2(100,100);
@@ -11,7 +10,7 @@ public class GameState{
     public int points = 0;
     public float[] collegeHealth = {100,100};
     Random rand = new Random();
-    public Object[] collegeNames = {"langwith","james","goodricke"};
+    public List<String> collegeNames = Arrays.asList("langwith","james","goodricke");
     public Map<Object, float[]> collegeHealths = new HashMap<Object,float[]>();
     public Map<Object, Vector2> collegePositions = new HashMap<Object,Vector2>();
     public boolean isSpawn = true;
