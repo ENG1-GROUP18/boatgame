3e6885bae01e0a5d4329f5008bfb7ab5789e5851
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-05 11:08:21 +0000

    Update GameState.java

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -1,20 +1,22 @@
 package com.boatcorp.boatgame;
 import com.badlogic.gdx.math.Vector2;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
-public class GameState {
-    public Vector2 playerPosition;
-    public float currentHealth;
-    public float maxHealth;
-    public int points;
-
+public class GameState{
+    public Vector2 playerPosition = new Vector2(100,100);
+    public float currentHealth = 100;
+    public float maxHealth = 100;
+    public int points = 0;
+    public float[] collegeHealth = {100,100};
+    Random rand = new Random();
+    public Object[] collegeNames = {"langwith","james","goodricke"};
+	public Map<Object, float[]> collegeHealths = new HashMap<Object,float[]>();
+    public Map<Object, Vector2> collegePositions = new HashMap<Object,Vector2>();
     public GameState() {
-        this(new Vector2(100,100),100,100, 0);
+        for (int i = 0; i < 3; i++) {
+            collegeHealths.put(collegeNames[i], collegeHealth);
+            collegePositions.put(collegeNames[i], new Vector2(rand.nextInt(1200), rand.nextInt(1200)));
+        }}
     }
-    public GameState(Vector2 playerPosition,float currentHealth, float maxHealth, int points){
-        this.playerPosition = playerPosition;
-        this.currentHealth = currentHealth;
-        this.maxHealth = maxHealth;
-        this.points = points;
-
-    }
-}
