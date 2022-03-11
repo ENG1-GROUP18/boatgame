7cf645ede2d995bb7ef19283f51da0b35816877f
ra1286 <94614531+ra1286@users.noreply.github.com>
2022-03-11 03:37:51 +0000

    WIP just pushing for meeting tomorrow

--- core/src/com/boatcorp/boatgame/entities/Projectile.java
+++ core/src/com/boatcorp/boatgame/entities/Projectile.java
@@ -2,32 +2,57 @@ package com.boatcorp.boatgame.entities;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class Projectile extends Actor {
 
+    // units moved per second
+
+    private float speed;
+
+    // TODO Do we even need??? can just get from box2d
     private Vector2 position;
-    private Vector2 direction;
     private float rotation;
 
+    private static BodyDef bodydef = new BodyDef();
+    private Body body;
+
+    private static float lifespan;
+    private float lifespanTimer;
 
     private Sprite sprite;
 
-    public Projectile() {
+    public Projectile(Vector2 position, float angle) {
+        // TODO create new box2d Body
+
+        body.setTransform(position, angle);
+        float velX = MathUtils.cos(angle);
+        float velY = MathUtils.sin(angle);
+        body.setLinearVelocity(velX, velY);
+    }
 
 
+    @Override
+    public void act(float delta) {
+        super.act(delta);
 
+        // do we even need this? probably not
+        position = body.getPosition();
+
+        lifespan += delta;
+        if (lifespanTimer > lifespan) {
+            // delete object
+        }
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-    }
-
-    @Override
-    public void act(float delta) {
-        super.act(delta);
+        sprite.draw(batch);
     }
 }
--- core/src/com/boatcorp/boatgame/entities/newCollege.java
+++ core/src/com/boatcorp/boatgame/entities/newCollege.java
@@ -1,37 +1,102 @@
 package com.boatcorp.boatgame.entities;
 
-import java.awt.geom.Point2D;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 
-public class newCollege {
+import java.awt.geom.Point2D;
+import java.util.*;
+
+public class newCollege extends Actor {
 
     //TODO notes:
-    // look into using JSON to store attack patterns
-    // or just txt file
+    // look into using JSON to store info about each college, have loader that creates colleges
+    // LOOK INTO EVENTS, LISTENERS etc etc
 
-    ArrayList<ArrayList<Integer>> attackPatternPool= new ArrayList<>();
+    private Vector2 position;
+    private boolean inCombat;
 
+    private Sprite sprite;
+    private Body body;
 
-    List<Float> pattern1 = Arrays.asList(1f, 2f, 3f, 4f, 5f);
-    List<Float> pattern2 = Arrays.asList(1f, 2f, 3f, 4f, 5f);
-    List<Float> pattern3 = Arrays.asList(1f, 2f, 3f, 4f, 5f);
-    List<Float> pattern4 = Arrays.asList(1f, 2f, 3f, 4f, 5f);
-
-
-    public newCollege() {
+    private ArrayList<attackPattern> attackPatternPool = new ArrayList<>();
 
 
+    public newCollege(Vector2 position) {
+        this.position = position;
 
     }
 
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+
+        // TODO check if player in range
+
+
+        if (inCombat) {
+            attack();
+        }
+    }
+
+    private void attack() {
+        // Move on to next attack pattern?
+
+
+    }
 
     private void addAttackPattern() {
 
     }
 
+    private class attackPattern {
+
+        boolean aimAtPlayer;
+        float interval;
+        int[] angles;
+
+        private void addAttackPattern(float interval, int[] angles) {
+
+
+        }
+
+        private void attack() {
+            for (int i = 0 ; i < angles.length; i++) {
+                new Projectile(position, angles[i]);
+            }
+
+        }
+    }
+
+    private class patternPool {
+
+        ArrayList<attackPattern> attackPatternPool= new ArrayList<>();
+        Iterator<attackPattern> it = attackPatternPool.iterator();
+        private int counter;
+
+
+
+        private void patternPool(float interval, int[] angles) {
+
+
+        }
+
+        private void addPattern() {
+
+        }
+
+        private void attack() {
+
+        }
+
+    }
 
 
 }
