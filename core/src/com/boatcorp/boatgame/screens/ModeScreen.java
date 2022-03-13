a9b65125f6fee211cbd4b3332408d7890e92d6ef
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-13 18:14:59 +0000

    Create ModeScreen.java

---
+++ core/src/com/boatcorp/boatgame/screens/ModeScreen.java
@@ -0,0 +1,80 @@
+package com.boatcorp.boatgame.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.boatcorp.boatgame.BoatGame;
+
+public class ModeScreen extends BasicMenuScreen{
+
+
+    public ModeScreen(final BoatGame game) {
+        super(game);
+
+        // Create table
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(game.ENABLE_TABLE_DEBUG);
+        stage.addActor(table);
+
+        // Add labels to table
+        Label label1 = new Label("[NORMAL]play on easy with [HIGHLIGHTED]E", style);
+        label1.setAlignment(Align.center);
+        label1.setFontScale(1f);
+
+        Label label2 = new Label("[NORMAL]play on normal with [HIGHLIGHTED]N", style);
+        label2.setAlignment(Align.center);
+        label2.setFontScale(1f);
+
+        Label label3 = new Label("[NORMAL]play on hard with [HIGHLIGHTED]H", style);
+        label3.setAlignment(Align.center);
+        label3.setFontScale(1f);
+
+
+        table.add(label1).fillX().uniformX().pad(20).row();
+        table.add(label2).fillX().uniformX().pad(20).row();
+        table.add(label3).fillX().uniformX().pad(20);
+
+        stage.addListener(new InputListener(){
+            public boolean keyDown(InputEvent event, int keycode){
+                if (keycode == Input.Keys.E) {
+                    game.difficulty = 0;
+                    game.changeScreen(BoatGame.screenType.PLAY);
+                }
+                return true;
+            }
+        });
+
+        stage.addListener(new InputListener(){
+            public boolean keyDown(InputEvent event, int keycode){
+                if (keycode == Input.Keys.N) {
+                    game.difficulty = 1;
+                    game.changeScreen(BoatGame.screenType.PLAY);
+                }
+                return true;
+            }
+        });
+
+        stage.addListener(new InputListener(){
+            public boolean keyDown(InputEvent event, int keycode){
+                if (keycode == Input.Keys.H) {
+                    game.difficulty = 2;
+                    game.changeScreen(BoatGame.screenType.PLAY);
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+}
