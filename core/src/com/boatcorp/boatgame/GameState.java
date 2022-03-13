f24fc2ea98a79b2930944bcd87ce5bcb973404a7
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-13 17:29:08 +0000

    Update GameState.java

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -17,7 +17,7 @@ public class GameState{
     public float damageScaler = 1f;
     public float[] collegeHealth = {100,100};
     public int difficulty = 1;
-    public ArrayList<String> collegeNames = new ArrayList<>( Arrays.asList("langwith","james","goodricke"));
+    public ArrayList<String> collegeNames = new ArrayList<>( Arrays.asList("langwith","james","goodricke","alcuin","derwent","halifax"));
     public Map<Object, float[]> collegeHealths = new HashMap<Object,float[]>();
     public Map<Object, Vector2> collegePositions = new HashMap<Object,Vector2>();
     public boolean isSpawn = true;
