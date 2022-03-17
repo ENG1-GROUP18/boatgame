57b3b8e8a0605d290f153dfa1690c5907a477877
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-17 21:30:55 +0000

    added health macro

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -19,6 +19,7 @@ public class GameState{
     public int difficulty = 1;
     public boolean hasBoughtGreen = true;
     public boolean hasBoughtRed = true;
+    public boolean hasBoughtHealth = true;
     public ArrayList<String> collegeNames = new ArrayList<>( Arrays.asList("langwith","james","goodricke","alcuin","derwent","halifax"));
     public Map<Object, float[]> collegeHealths = new HashMap<Object,float[]>();
     public Map<Object, Vector2> collegePositions = new HashMap<Object,Vector2>();
