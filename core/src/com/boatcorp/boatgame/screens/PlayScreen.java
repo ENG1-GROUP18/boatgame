713f0ac533cc83e6d604d67336afae6cb5a637f2
Morgan-Elkins <me839@york.ac.uk>
2022-03-18 13:44:57 +0000

    Fixed error on screen reload Put back code which I accidentally removed

--- core/src/com/boatcorp/boatgame/screens/PlayScreen.java
+++ core/src/com/boatcorp/boatgame/screens/PlayScreen.java
@@ -174,6 +174,8 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         vfxManager.cleanUpBuffers();
         vfxManager.beginInputCapture();
 
