8b96c28c801722f9277fd5cc39715fea5291c23a
Sam <62548353+sk1735@users.noreply.github.com>
2022-03-18 09:37:13 +0000

    Update GameState.java

--- core/src/com/boatcorp/boatgame/GameState.java
+++ core/src/com/boatcorp/boatgame/GameState.java
@@ -15,11 +15,13 @@ public class GameState{
     public int plunder = 0;
     public int immuneSeconds = 0;
     public float damageScaler = 1f;
+    public float shipDamageScaler = 1;
     public float[] collegeHealth = {100,100};
     public int difficulty = 1;
     public boolean hasBoughtGreen = false;
     public boolean hasBoughtRed = false;
     public boolean hasBoughtHealth = false;
+    public String bulletColor = "bullet";
     public ArrayList<String> collegeNames = new ArrayList<>( Arrays.asList("langwith","james","goodricke","alcuin","derwent","halifax"));
     public Map<Object, float[]> collegeHealths = new HashMap<Object,float[]>();
     public Map<Object, Vector2> collegePositions = new HashMap<Object,Vector2>();
