0ebb81f811046401674da424f60281128b9f782c
scottredshaw <scott.redshaw10@gmail.com>
2022-04-23 16:03:11 +0100

    Creating and deleting of the unit test screens

---
+++ core/src/test/java/com/boatcorp/boatgame/screens/SplashScreenTest.java
@@ -0,0 +1,72 @@
+package com.boatcorp.boatgame.screens;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.graphics.GL20;
+import com.boatcorp.boatgame.BoatGame;
+import com.boatcorp.boatgame.GameState;
+import junit.framework.TestCase;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertNotNull;
+
+public class SplashScreenTest{
+    private SplashScreen screen;
+    public static Application app;
+    private static GameState state;
+
+    @BeforeClass
+    public static void setUp(){
+        //Any set up for tests go here
+        app = new HeadlessApplication(
+                new ApplicationListener() {
+                    @Override
+                    public void create() {}
+                    @Override
+                    public void resize(int width, int height) {}
+                    @Override
+                    public void render() {}
+                    @Override
+                    public void pause() {}
+                    @Override
+                    public void resume() {}
+                    @Override
+                    public void dispose() {}
+                }
+        );
+        Gdx.gl20 = Mockito.mock(GL20.class);
+        Gdx.gl = Gdx.gl20;
+        Gdx.graphics = Mockito.mock(com.badlogic.gdx.Graphics.class);
+    }
+
+    @Before
+    public void start(){
+        BoatGame boatGameInstance = new BoatGame();
+        boatGameInstance.setHeadless();
+        screen = new SplashScreen(boatGameInstance);
+    }
+
+    @Test
+    public void SplashScreenCreated(){
+        screen.resize(5,5);
+        assertNotNull("Splash screen not initialised",screen);
+    }
+
+    @Test
+    public void disposeTest(){
+        screen.dispose();
+        assertNotNull("Splash Screen not initialised",screen);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        app.exit();
+        app = null;
+    }
+}
